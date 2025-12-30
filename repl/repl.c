3fd6c25a6a05522363bece0170cf99458c8445ba
Ivan Efremov <welcomebrianemusp@gmail.com>
2025-12-30 09:12:56 +0700

    fixed bug

--- repl/repl.c
+++ repl/repl.c
@@ -43,13 +43,13 @@ int main(int argc, char *argv[]) {
   InputBuffer *input_buffer = new_input_buffer();
   while (true) {
     print_prompt();
-  }
-
-  // здесь пишем команды для repl
-  if ((strcmp(input_buffer->buffer, ".exit")) == 0) {
-    close_input_buffer(input_buffer);
-    exit(EXIT_SUCCESS);
-  } else {
-    printf("unrecognized command '%s'.\n", input_buffer->buffer);
+    read_input(input_buffer);
+    // здесь пишем команды для repl
+    if ((strcmp(input_buffer->buffer, ".exit")) == 0) {
+      close_input_buffer(input_buffer);
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("unrecognized command '%s'.\n", input_buffer->buffer);
+    }
   }
 }
